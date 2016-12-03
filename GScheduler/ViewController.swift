7f0fdd17403c064076c1696a0fb2dc046059956a
crea <masataka0504@gmail.com>
2016-12-03 12:54:52 -0800

    Initial Commit

---
+++ GScheduler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GScheduler
+//
+//  Created by mas.t on 2016/12/03.
+//  Copyright © 2016年 masa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
